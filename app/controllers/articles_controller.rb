3c61ddcf8ef092492060bc81cead5ad2b44e4f82
konyan <konyan2018@gmail.com>
2016-02-14 20:23:40 +0630

    Add edit,Show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,31 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@article = Article.all
+	end
+
+
 	def new
 		@article  = Article.new
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+		
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article is Successfully Update."
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+		
+	end
+	
+
 
 	def create
 		
@@ -21,6 +44,7 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
 	private
 		def article_params
 			params.require(:article).permit(:title,:description)
