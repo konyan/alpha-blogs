0405d6b7a24320f975cf78830627a399243d4fbc
Ko Nyan <konyan2018@gmail.com>
2016-02-21 13:10:41 +0630

    User validation and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
 
+	validates :username, presence: true,
+				uniqueness:{case_sensitive: false},
+				length: {mininum: 3 , maximum: 25}
+
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true,
+				uniqueness:{case_sensitive: false},
+				format: {with: VALID_EMAIL_REGEX},
+				length: {maximum: 50}			
+
 end 
